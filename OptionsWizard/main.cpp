6ad41f2b8be70e318485a27fe7f6616732cdfc7a
rhyde23 <reginaldjhyde@gmail.com>
2024-08-05 14:24:48 -0500

    Added user input error handling for menu for selecting pricing model.

--- OptionsWizard/main.cpp
+++ OptionsWizard/main.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 using namespace std;
 
 //Main function
-int main()
+int main(int argc, char* argv[])
 {
 
     //Options Wizard title!
@@ -27,19 +28,47 @@ int main()
     while (running) {
 
         //Menu
-        cout << "Enter a number to select one of the following pricing models" << endl;
+       
 
-        for (int i = 1; i <= models.size(); ++i) {
-            cout << i << endl;
+        try {
+            cout << "Enter a number to select one of the following pricing models" << endl;
+
+            for (int i = 1; i <= models.size(); ++i) {
+                cout << i;
+                cout << ": " + models[i - 1] << endl;
+            }
+
+            cout << ">> ";
+
+            cin >> model_choice;
+
+            // Check for input failure
+            if (cin.fail()) {
+                throw invalid_argument("Invalid input. Not an integer.");
+            }
+
+            // Clear the error state if input is valid
+            cin.clear();
+            cin.ignore(10000, '\n'); // Discard invalid input up to 10,000 characters
+
+            // If we reach this point, the input is an integer
+
+            if (model_choice <= models.size()) {
+                selected_model = models[model_choice - 1];
+                cout << selected_model << endl;
+                break; // Exit the loop
+            }
+            else {
+                cout << "Invalid input. Enter a number between 1 and ";
+                cout << models.size() << endl;
+                cout << "" << endl;
+            }
         }
-
-        cout << ">>";
-
-        cin >> model_choice;
-
-        selected_model = models[model_choice-1];
-
-        cout << selected_model << endl;
+        catch (const invalid_argument& e) {
+            cout << e.what() << std::endl; // Display the error message
+            cin.clear(); // Clear the error flag
+            cin.ignore(10000, '\n'); // Discard invalid input
+        }
     }
 
     return 0;
